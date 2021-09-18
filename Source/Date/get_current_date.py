8f9d948671a38f929df5f8f5d2ca1c8b019aff55
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-18 20:44:33 +0200

    Added "Get current date" snippet

---
+++ Source/Date/get_current_date.py
@@ -0,0 +1,17 @@
+# Get the current date
+
+import datetime
+
+
+def main():
+    current_date = datetime.datetime.now()
+
+    year = current_date.year
+    month = current_date.month
+    day = current_date.day
+
+    print(f"{day}/{month}/{year}")
+
+
+if __name__ == "__main__":
+    main()
