fb281c64d09861bfccca726aa2eaf39aabb5c38f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-28 16:36:06 +0200

    Added "circle" code snippet

---
+++ Source/Turtle/circle.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+
+def main():
+    color("black", "black")
+    speed(0)
+
+    begin_fill()
+
+    for i in range(380):
+        forward(1)
+        left(1)
+
+    end_fill()
+    done()
+
+
+if __name__ == "__main__":
+    main()
