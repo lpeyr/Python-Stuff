34ac2d488c5ac05e1da89bef3f5d984a2b4b723a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-28 16:30:30 +0200

    Added "square" code snippet

---
+++ Source/Turtle/square.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+
+def main():
+    color("black", "black")
+
+    begin_fill()
+
+    for i in range(4):
+        forward(200)
+        left(90)
+
+    end_fill()
+    done()
+
+
+if __name__ == "__main__":
+    main()
