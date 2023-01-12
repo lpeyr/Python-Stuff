28bf55a8f388fe5d32749ee130048a30739d01dc
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-01-12 17:32:02 +0100

    Create military_alpha.py

---
+++ Source/Misc/military_alpha.py
@@ -0,0 +1,35 @@
+alphabet = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliette",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-Ray",
+    "Y": "Yankee",
+    "Z": "Zulu",
+}
+
+text = input("Enter text\n> ")
+s = ""
+for c in text:
+    s += f"{alphabet[c.upper()]} "
+
+print(s.upper())
