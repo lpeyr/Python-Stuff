2a874bde354db96e45fed8a3e2abfc79fa8b1913
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-05-18 17:07:45 +0200

    Removed useless function

--- FR/C6-7-8/dm_dictionnaire.py
+++ FR/C6-7-8/dm_dictionnaire.py
@@ -16,25 +16,12 @@ def transformer(texte: str) -> str:
     """
     final: str = ""  # texte final
     for lettre in texte:  # pour chaque lettre
-        if est_nbr(lettre):  # si la lettre est un nombre, alors on transforme le chiffre en lettre
+        # si la lettre est un nombre, alors on transforme le chiffre en lettre
+        if lettre in [str(c) for c in nb]:
             final += nb[int(lettre)]
         else:  # si la lettre n'est pas un nombre, on ajoute cette lettre à la chaîne finale.
             final += lettre
     return final  # Renvoie le texte transformée
-
-
-def est_nbr(lettre: str) -> bool:
-    """
-    Description
-        Vérifie si un caractère est un chiffre contenu (en tant que clé) dans le dictionnaire nb.
-
-    Argument:
-        lettre (str): La lettre à vérifier.
-
-    Renvoie:
-        bool: True si la lettre est un chiffre, False si la lettre n'est pas un chiffre.
-    """
-    return lettre in [str(c) for c in nb]  # obtient un tableau contenant les clés du dictionnaire nb sous forme de chaîne de caractère (1 devient "1") et vérifie si la lettre en question est à l'intérieur
 
 
 # Tests
